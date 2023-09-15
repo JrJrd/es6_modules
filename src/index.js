50134ab723609396b109fcdfec582d2dd4a45826
seaer <jrd.seaer@gmail.com>
2023-09-15 19:02:49 +0300

    details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 import { WishList } from "./wishlist.js";
-import { Car } from "./car.js";
 
 let makeInput = document.getElementById("makeInput");
 let form = document.getElementById("submitForm");
@@ -15,9 +14,6 @@ let wishlistUl = document.querySelector("#wishListContainer ul");
 
 let wishlist = new WishList();
 
-form.addEventListener("submit", addCar);
-removeBtn.addEventListener("click", removeCar);
-
 function showCarDetails(car) {
   makeDisplay.textContent = car.make;
   modelDisplay.textContent = car.model;
@@ -27,37 +23,41 @@ function showCarDetails(car) {
 }
 
 function updateDOMList() {
-    wishlistUl.innerHTML = "";
-    wishlist.list.forEach((car) => {
-      const li = document.createElement("li");
-      li.textContent = `${car.make} ${car.model}`;
-      li.addEventListener("click", () => showCarDetails(car));
-      wishlistUl.appendChild(li);
-    });
-  }
+  wishlistUl.innerHTML = "";
+  wishlist.list.forEach((car) => {
+    const li = document.createElement("li");
+    li.textContent = `${car.make} ${car.model}`;
+    li.addEventListener("click", () => showCarDetails(car));
+    wishlistUl.appendChild(li);
+  });
+}
   
-  function addCar(event) {
+function addCar(event) {
   
   event.preventDefault();
-
+  
   let make = makeInput.value;
   let model = modelInput.value;
   let year = yearInput.value;
-
+  
   wishlist.add(make, model, year);
   
-
+  
   updateDOMList();
 }
 
 function removeCar() {
-    let carId = Number(removeBtn.getAttribute("data-carId"));
-    wishlist.remove(carId);
+  let carId = Number(removeBtn.getAttribute("data-carId"));
+  wishlist.remove(carId);
   
-    updateDOMList();
+  updateDOMList();
   
-    makeDisplay.textContent = "";
-    modelDisplay.textContent = "";
-    yearDisplay.textContent = "";
-    removeBtn.disabled = true;
-  }
+  makeDisplay.textContent = "";
+  modelDisplay.textContent = "";
+  yearDisplay.textContent = "";
+  removeBtn.disabled = true;
+}
+
+
+form.addEventListener("submit", addCar);
+removeBtn.addEventListener("click", removeCar);
